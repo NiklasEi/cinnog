2fb983f57743459725ce93783d0323feb5183258
Niklas Eicker <git@nikl.me>
2023-12-25 22:47:29 +0100

    Make markdown loader recursive

--- src/loaders.rs
+++ src/loaders.rs
@@ -8,33 +8,51 @@ use gray_matter::Matter;
 use leptos::serde::de::DeserializeOwned;
 use pulldown_cmark::{html, Options, Parser};
 use std::fs::{read_to_string, File};
+use std::path::PathBuf;
 use std::{fs, io};
 
 pub fn read_markdown_from_directory<'a, FrontMatter: ToBundle + DeserializeOwned>(
     In(path): In<&'a str>,
     mut commands: Commands,
 ) -> io::Result<Vec<Entity>> {
-    let mut files = vec![];
-    for entry in fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_file() {
-            let file = read_to_string(path.clone())?;
-            let matter = Matter::<YAML>::new();
-            let markdown = matter.parse(&file);
-            let mut file = commands.spawn(());
-            if let Some(front_matter) = markdown.data {
-                file.insert(
-                    front_matter
-                        .deserialize::<FrontMatter>()?
-                        .to_bundle_with_path(&path),
-                );
+    fn read_from_dir<FrontMatter: ToBundle + DeserializeOwned>(
+        path: &PathBuf,
+        commands: &mut Commands,
+    ) -> io::Result<Vec<Entity>> {
+        let mut files = vec![];
+
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_file() {
+                files.push(read_markdown::<FrontMatter>(&path, commands)?);
+            } else if path.is_dir() {
+                files.append(&mut read_from_dir::<FrontMatter>(&path, commands)?)
             }
-            file.insert(MarkdownBody(markdown.content));
-            files.push(file.id());
         }
+        Ok(files)
     }
-    Ok(files)
+    let path = path.into();
+    Ok(read_from_dir::<FrontMatter>(&path, &mut commands)?)
+}
+
+fn read_markdown<'a, FrontMatter: ToBundle + DeserializeOwned>(
+    path: &PathBuf,
+    commands: &mut Commands,
+) -> io::Result<Entity> {
+    let file = read_to_string(path.clone())?;
+    let matter = Matter::<YAML>::new();
+    let markdown = matter.parse(&file);
+    let mut file = commands.spawn(());
+    if let Some(front_matter) = markdown.data {
+        file.insert(
+            front_matter
+                .deserialize::<FrontMatter>()?
+                .to_bundle_with_path(&path),
+        );
+    }
+    file.insert(MarkdownBody(markdown.content));
+    Ok(file.id())
 }
 
 pub fn mark_with<C: Component + Default>(In(entities): In<Vec<Entity>>, mut commands: Commands) {
