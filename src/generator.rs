40e40d9c3e0780af4e31da0ee7ecdc8eac65919f
Niklas Eicker <git@nikl.me>
2024-04-20 21:37:05 +0200

    Full builder pattern

--- src/generator.rs
+++ src/generator.rs
@@ -16,14 +16,15 @@ use tower_http::services::ServeDir;
 
 impl DataLayer {
     pub async fn build<IV>(
-        mut self,
+        &mut self,
         app_fn: impl Fn() -> IV + Clone + Send + 'static,
     ) -> std::io::Result<()>
     where
         IV: IntoView + 'static,
     {
         self.app.update();
-        let data = Arc::new(Mutex::new(self));
+        let datalayer = std::mem::replace(self, DataLayer::new());
+        let data = Arc::new(Mutex::new(datalayer));
         let data_for_route_generation = data.clone();
 
         let conf = get_configuration(None).await.unwrap();
