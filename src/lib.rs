f6b6b3e4de9a5534c373f75e4d81548769231e52
Niklas Eicker <git@nikl.me>
2024-12-01 19:33:57 +0100

    Basic inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 #![forbid(unsafe_code)]
 #![deny(missing_docs)]
-///! Cinnog is an experimental static site generator using [Leptos](https://www.leptos.dev/)
-///!
-///! ## Datalayer
-///! Cinnog uses Bevy ECS as [`Datalayer`] to simplify site data preparation and handling.
-///! Before building the site, you can add any data into the datalayer or manipulate already
-///! exising data.
-///! During site generation, your Leptos components can query the data layer and generate
-///! content based on it. Example workflows are automatic conversion of markdown files to HTML,
-///! or resizing all media content.
+//! Cinnog is an experimental static site generator using [Leptos](https://www.leptos.dev/)
+//!
+//! ## Datalayer
+//! Cinnog uses Bevy ECS as [`Datalayer`] to simplify site data preparation and handling.
+//! Before building the site, you can add any data into the datalayer or manipulate already
+//! exising data.
+//! During site generation, your Leptos components can query the data layer and generate
+//! content based on it. Example workflows are automatic conversion of markdown files to HTML,
+//! or resizing all media content.
+
 mod datalayer;
+/// Filling and preparing the datalayer for site generation.
 #[cfg(feature = "generator")]
 pub mod generator;
+/// Load and convert different types of files.
 pub mod loaders;
 mod world;
 
@@ -25,6 +28,9 @@ use std::any::type_name;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 
+/// Expect a resource from the data layer.
+///
+/// Panics when the resource cannot be found. See [`use_resource`] for optional resources.
 pub fn expect_resource<R: Resource + Clone>() -> R {
     use_resource::<R>().unwrap_or_else(|| {
         panic!(
@@ -34,6 +40,7 @@ pub fn expect_resource<R: Resource + Clone>() -> R {
     })
 }
 
+/// Execute a system against the datalayer.
 pub fn run_system<S, R, T>(system: S) -> R
 where
     S: IntoSystem<(), R, T> + 'static,
@@ -42,6 +49,7 @@ where
     run_system_with_input(system, ())
 }
 
+/// Execute a system with given inputs against the datalayer.
 pub fn run_system_with_input<S, I, R, T>(system: S, input: I::Inner<'_>) -> R
 where
     S: IntoSystem<I, R, T> + 'static,
@@ -54,26 +62,29 @@ where
     data_layer.run(system, input)
 }
 
+/// Use a resource from the data layer.
 pub fn use_resource<R: Resource + Clone>() -> Option<R> {
     let cinnog = expect_context::<Arc<Mutex<Datalayer>>>();
     let lock = cinnog.lock().unwrap();
     lock.get_resource()
 }
 
+/// The name of the file that this entity represents.
 #[derive(Component, Clone, Debug)]
 pub struct FileName(pub String);
 
-#[derive(Component, Clone, Debug)]
-pub struct StaticPath(pub String);
-
+/// The path of the file that this entity represents.
 #[derive(Component, Clone, Debug)]
 pub struct FilePath(pub String);
 
+/// Define how to convert data into ECS Components in the datalayer.
 pub trait Ingest {
+    /// Ingest data into the Datalayer by converting it into ECS Components.
     fn ingest(self, commands: &mut EntityCommands)
     where
         Self: Sized;
 
+    /// Ingest standard ECS Components relating to a file path.
     fn ingest_path(&self, commands: &mut EntityCommands, path: &Path) {
         commands.insert(default_bundle_from_path(path));
     }
--- src/world.rs
+++ src/world.rs
@@ -1,16 +1,19 @@
 use bevy_ecs::bundle::Bundle;
-use bevy_ecs::prelude;
 use bevy_ecs::prelude::{EntityWorldMut, SystemInput};
-use bevy_ecs::system::IntoSystem;
+use bevy_ecs::system::{IntoSystem, Resource};
 
+/// Access to the inner [`bevy_ecs::world::World`] of the datalayer.
 pub trait DataWorld {
+    /// Run a given system with inputs against the [`bevy_ecs::world::World`].
     fn run<S, In, Out, Marker>(&mut self, system: S, input: In::Inner<'_>) -> Out
     where
         S: IntoSystem<In, Out, Marker> + 'static,
         Out: 'static,
         In: SystemInput + 'static;
 
-    fn get_resource<R: prelude::Resource + Clone>(&self) -> Option<R>;
+    /// Get a [`Resource`] form the [`bevy_ecs::world::World`].
+    fn get_resource<R: Resource + Clone>(&self) -> Option<R>;
 
+    /// Spawn a new entity into the [`bevy_ecs::world::World`].
     fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityWorldMut;
 }
